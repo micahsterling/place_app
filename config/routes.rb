37e09f7e05b18e12373da0ed44889845c1b4c426
Micah Sterling <msterling89@gmail.com>
2021-03-31 18:32:18 -0500

    add update action

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   namespace :api do
     get "/places" => "places#index"
     post "/places" => "places#create"
+    patch "/places/:id" => "places#update"
   end
 end
